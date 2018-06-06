bbe787eea4ab51c6b670e3a885b077259f57ec1c
kinghao <kinghao1020437855@163.com>
2018-06-06 21:42:28 +0800

    add search function

--- blog/article/views.py
+++ blog/article/views.py
@@ -4,6 +4,19 @@ from article.models import Article
 from datetime import datetime
 
 # Create your views here.
+def blog_search(request):
+    if 's' in request.GET:
+        s = request.GET['s']
+        if not s:
+            return render(request, 'home.html')
+        else:
+            post_list = Article.objects.filter(title__icontains = str(s))
+            if len(post_list) == 0:
+                 return render(request, 'archives.html', {'post_list': post_list, 'error': True})
+            else:
+                 return render(request, 'archives.html', {'post_list': post_list, 'error': False})
+    return redirect('/')
+
 def search_tag(request, tag):
     try:
         post_list = Article.objects.filter(category=tag)
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
     url(r'^test/$', views.test),
     url(r'^archives/$', views.archives, name='archives'),
-    url(r'^aboutme/$', views.about_me, name='aboutme'),
+    url(r'^aboutme/$', views.about_me, name='about_me'),
     url(r'^tag/(?P<tag>\w+)/$', views.search_tag, name='search_tag'),
+    url(r'^search/$', views.blog_search, name='search'),
 ]
