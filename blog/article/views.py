82d53f40948b252169342f089b00daa6169f8d68
kinghao <kinghao1020437855@163.com>
2018-06-06 20:32:20 +0800

    add new aboutme, tag..

--- blog/article/views.py
+++ blog/article/views.py
@@ -4,6 +4,16 @@ from article.models import Article
 from datetime import datetime
 
 # Create your views here.
+def search_tag(request, tag):
+    try:
+        post_list = Article.objects.filter(category=tag)
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'tag.html', {'post_list': post_list})
+
+def about_me(request):
+    return render(request, 'aboutme.html')
+
 def home(request):
     post_list = Article.objects.all()
     return render(request, 'home.html', {'post_list': post_list})
@@ -17,3 +27,10 @@ def detail(request, id):
 
 def test(request):
     return render(request, 'test.html', {'current_time': datetime.now()})
+
+def archives(request):
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'archives.html', {'post_list': post_list, 'error': False})
--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
     url(r'^test/$', views.test),
+    url(r'^archives/$', views.archives, name='archives'),
+    url(r'^aboutme/$', views.about_me, name='aboutme'),
+    url(r'^tag/(?P<tag>\w+)/$', views.search_tag, name='search_tag'),
 ]
